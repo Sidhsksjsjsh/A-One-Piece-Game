21aedb900200458d61080ecf9fd46d104e12c9e3
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-03-30 18:14:31 +0900

    Update .lua

--- .lua
+++ .lua
@@ -1,6 +1,8 @@
 local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sidhsksjsjsh/VAPE-UI-MODDED/main/.lua"))()
 local wndw = lib:Window("VIP Turtle Hub V4")
 local T1 = wndw:Tab("Main")
+local T2 = wndw:Tab("Teleport")
+local T75 = wndw:Tab("Note")
 
 local workspace = game:GetService("Workspace")
 local self = game:GetService("Players").LocalPlayer
@@ -13,8 +15,17 @@ local var = {
     dmg = 9e9
   },
   spin = false,
-  dash = false
+  dash = false,
+  island = {},
+  szone = "null",
+  method = "tween"
 }
+--lib:TeleportMethod(mthd,str)
+lib:AddTable(workspace["Map"]["Islands"],var.island)
+
+T75:Label("This script is still in " .. lib:ColorFonts("development stage","Red") .. "\nSome features are upcoming and bugs will be " .. lib:ColorFonts("fixed","Red") .. " soon.")
+T75:Label("Currently the 'Auto attack' feature is still available for melee weapons\nfor fruit and others will come in future updates.")
+T75:Label("DONT COMPLAIN BITCH")
 
 local function getEnemy(near,num,funct)
 for i,v in pairs(workspace["Entities"]:GetChildren()) do
@@ -51,5 +62,19 @@ T1:Toggle("Auto dash",false,function(value)
     while wait() do
       if var.dash == false then break end
       game:GetService("ReplicatedStorage")["Remotes"]["requestDash"]:InvokeServer("W")
+    end
+end)
+
+T2:Dropdown("Select island",var.island,function(value)
+    var.szone = value
+end)
+
+T2:Dropdown("Select tp method",{"tween","tp"},function(value)
+    var.method = value
+end)
+
+T2:Button("Go to selected island",function()
+    if var.szone ~= "null" then
+      lib:TeleportMethod(var.method,workspace["Map"]["Islands"][var.szone]["Model"]["Spawner"]["Crystal"]["CFrame"])
     end
 end)
